45c417720122c2d23934209d57087ad924f6247f
Cleitomar <cleitomarrodrigues@gmail.com>
2026-08-04 13:40:40 -0300

    banco

--- backend/docker/wait-for-db.php
+++ backend/docker/wait-for-db.php
@@ -4,6 +4,6 @@ $host = getenv('DB_HOST') ?: 'mysql';
 $port = getenv('DB_PORT') ?: '3306';
 $database = getenv('DB_DATABASE') ?: 'wiki_relacionamento';
 $user = getenv('DB_USERNAME') ?: 'root';
-$password = getenv('DB_PASSWORD') ?: 'root';
+$password = getenv('DB_PASSWORD') ?: 'C2f0z2@6';
 
 new PDO("mysql:host={$host};port={$port};dbname={$database}", $user, $password);
